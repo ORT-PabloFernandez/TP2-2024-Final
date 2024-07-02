60b23479aaeaa48571eb3601d74057c002c6b60d
Ezequiel Cherone <emcherone@teco.com.ar>
2024-07-01 21:58:37 -0300

    Cherone Ezequiel Matias

--- data/sales.js
+++ data/sales.js
@@ -1,4 +1,6 @@
 import getConnection from "./conn.js";
+import { ObjectId } from 'mongodb';
+
 const DATABASE = "sample_supplies";
 const MOVIES = "sales";
 
@@ -14,4 +16,57 @@ async function getAllSales(pageSize, page) {
   return sales;
 }
 
-export { getAllSales };
+async function getSalesById(salesId) {
+  const connectiondb = await getConnection();
+  const sales = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .findOne({ _id: new ObjectId(salesId) });
+  return sales;
+}
+
+async function getSalesByStoreLocation(salesStoreLocation) {
+  const connectiondb = await getConnection();
+  const sales = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({ storeLocation: salesStoreLocation })
+    .toArray();
+  return sales;
+}
+
+
+async function getSalesByFilters(storeLocation, purchaseMethod, couponUsed) {
+  const connectiondb = await getConnection();
+  const query = {};
+
+  if (storeLocation) {
+    query.storeLocation = storeLocation;
+  }
+  if (purchaseMethod) {
+    query.purchaseMethod = purchaseMethod;
+  }
+  if (couponUsed) {
+    query.couponUsed = couponUsed === 'true';
+  }
+
+  const sales = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find(query)
+    .toArray();
+  return sales;
+}
+
+async function getSalesByClientSatisfaction(typeSatisfaction) {
+  const connectiondb = await getConnection();
+
+  const sales = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({ satisfaction: typeSatisfaction })
+    .toArray();
+  return sales;
+}
+
+export { getAllSales, getSalesById, getSalesByStoreLocation, getSalesByFilters, getSalesByClientSatisfaction };
